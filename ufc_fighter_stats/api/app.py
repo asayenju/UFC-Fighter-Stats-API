8ca0ef3f1e8e0af6392345d33e0fb2e2d1ea14df
ashwinsayenju@gmail.com <ashwinsayenju@gmail.com>
2024-06-11 13:40:05 +0700

    Resolved issue of api.routes not found

--- ufc_fighter_stats/api/app.py
+++ ufc_fighter_stats/api/app.py
@@ -1,4 +1,9 @@
 from flask import Flask
+import os
+import sys
+
+# Append the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from api.routes import fighters
 
 app = Flask(__name__)
